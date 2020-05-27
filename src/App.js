70119b6c8ca9a00ef0b7f1582507b26c27bcc5db
15101321280 <1925659630@qq.com>
2020-05-27 16:31:30 +0800

    按需引入组件

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Button from 'antd/lib/button';
 function App() {
   return (
     <div className="App">
       你好
+      <Button type="primary">Button</Button>
     </div>
   );
 }
