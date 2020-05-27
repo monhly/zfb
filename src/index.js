07957722b0a657882aae0473f821fe0c5aee571b
15101321280 <1925659630@qq.com>
2020-05-27 23:51:31 +0800

    引入字体图标

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// 导入字体图标的样式
+import "./assets/fonts/iconfont.css"
 // 导入路由
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,17 +1,134 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react'
 // 首页的组件
 // 导入路由的组件
-import { Route, Link } from "react-router-dom"
+import { TabBar } from 'antd-mobile';
+import { Route } from "react-router-dom"
 import HomeIndex from '../homeIndex'
 import House from "../House"
-import User from"../Users"
+import User from "../Users"
+
 export default class Home extends Component {
+    state = {
+        selectedTab: 'redTab',
+        hidden: false,
+        fullScreen: false,
+    }
+    renderContent(pageText) {
+        return (
+          <div style={{ backgroundColor: 'white', height: '100%', textAlign: 'center' }}>
+            <div style={{ paddingTop: 60 }}>Clicked “{pageText}” tab， show “{pageText}” information</div>
+            <a style={{ display: 'block', marginTop: 40, marginBottom: 20, color: '#108ee9' }}
+              onClick={(e) => {
+                e.preventDefault();
+                this.setState({
+                  hidden: !this.state.hidden,
+                });
+              }}
+            >
+              Click to show/hide tab-bar
+            </a>
+            <a style={{ display: 'block', marginBottom: 600, color: '#108ee9' }}
+              onClick={(e) => {
+                e.preventDefault();
+                this.setState({
+                  fullScreen: !this.state.fullScreen,
+                });
+              }}
+            >
+              Click to switch fullscreen
+            </a>
+          </div>
+        );
+      }
     render() {
         return (
             <div>
-                <Link to="/home/index">首页</Link>
-                <Link to="/home/house">fangyuan</Link>
-                <Link to="/home/user">个人</Link>
+                <div style={{ position: 'fixed', height: '100%', width: '100%', top: 0 } }>
+                <TabBar
+                    unselectedTintColor="#949494"
+                    tintColor="#33A3F4"
+                    barTintColor="white"
+                    hidden={this.state.hidden}
+                    >
+                    <TabBar.Item
+                        title="首页"
+                        key="Life"
+                        icon={
+                            <i className="iconfont icon-ind" />
+                          }
+                          selectedIcon={<i className="iconfont icon-ind" />
+                          }
+                        selected={this.state.selectedTab === 'blueTab'}
+                        badge={1}
+                        onPress={() => {
+                        this.setState({
+                            selectedTab: 'blueTab',
+                        });
+                        }}
+                        data-seed="logId"
+                    >
+                        {this.renderContent('Life')}
+                    </TabBar.Item>
+                    <TabBar.Item
+                        icon={
+                        <div style={{
+                            width: '22px',
+                            height: '22px',
+                            background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat' }}
+                        />
+                        }
+                        selectedIcon={
+                        <div style={{
+                            width: '22px',
+                            height: '22px',
+                            background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat' }}
+                        />
+                        }
+                        title="Koubei"
+                        key="Koubei"
+                        badge={'new'}
+                        selected={this.state.selectedTab === 'redTab'}
+                        onPress={() => {
+                        this.setState({
+                            selectedTab: 'redTab',
+                        });
+                        }}
+                        data-seed="logId1"
+                    >
+                        {this.renderContent('Koubei')}
+                    </TabBar.Item>
+                    <TabBar.Item
+                        icon={
+                        <div style={{
+                            width: '22px',
+                            height: '22px',
+                            background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat' }}
+                        />
+                        }
+                        selectedIcon={
+                        <div style={{
+                            width: '22px',
+                            height: '22px',
+                            background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat' }}
+                        />
+                        }
+                        title="Friend"
+                        key="Friend"
+                        dot
+                        selected={this.state.selectedTab === 'greenTab'}
+                        onPress={() => {
+                        this.setState({
+                            selectedTab: 'greenTab',
+                        });
+                        }}
+                    >
+                        {this.renderContent('Friend')}
+                    </TabBar.Item>
+
+                </TabBar>
+                </div>
+
                 {/* 我是首页 */}
                 {/* 设置二级路由 */}
                 {/* 重定向 */}
@@ -22,4 +139,5 @@ export default class Home extends Component {
             </div>
         )
     }
+
 }
