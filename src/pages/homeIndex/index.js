e255b68799e9617a0904212be2018f86e1de2399
15101321280 <1925659630@qq.com>
2020-05-28 17:35:10 +0800

    导航的区域

--- src/pages/homeIndex/index.js
+++ src/pages/homeIndex/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 // 导入轮播图的组件
-import { Carousel } from 'antd-mobile';
+import { Carousel ,Flex} from 'antd-mobile';
 //导入axios组件
 import { getLunbo } from "../../request/home"
 import { baseurl } from "../../utils/axios"
-
+// 导入样式
+import "./index.css"
+// 导入数据
+import navArr from"../../utils/navList"
 export default class HomeIndex extends Component {
     state = {
         data: [],
@@ -12,8 +15,7 @@ export default class HomeIndex extends Component {
         // 轮播图的状态
         statusLunBo:false
     }
-    // 获取请求
-
+    // 获取轮播请求
    async getLB () {
        const { description, status, body } = await getLunbo()
        //判断此时获取的数据状态
@@ -32,14 +34,12 @@ export default class HomeIndex extends Component {
      }
 
     }
-
-
+    // 组件渲染完成以后
     componentDidMount() {
         // simulate img loading
         // 获取请求
         this.getLB()
         console.log(baseurl);
-
     }
     // 轮播图的数据
     lunBo () {
@@ -72,12 +72,38 @@ export default class HomeIndex extends Component {
 
         )
     }
+    Nav () {
+        return (
+            <div>
+                <Flex className="nav">
+                    {
+
+                            //    <Flex.Item  key={i.id}>
+                            //     <img src={i.img} alt=""/>
+                            //     <p>{i.title}</p>
+                            //     </Flex.Item>
+
+                        navArr.map(i => {
+                            return (
+                            <Flex.Item  key={i.id}>
+                                 <img src={i.img} alt=""/>
+                                 <p>{i.title}</p>
+                            </Flex.Item>
+                            )
+                        })
+                    }
+            </Flex>
+            </div>
+        )
+    }
     render() {
         return (
             <div>
                 <div className="header">
                     {/* 轮播图的数据 */}
                     {this.lunBo()}
+                    {/* 导航栏的数据 */}
+                    {this.Nav()}
                 </div>
          </div>
         )
