793eda6ce54b87c948f16522664cbd103e60c953
15101321280 <1925659630@qq.com>
2020-05-29 21:55:10 +0800

    百度地图

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
 
 export default class Map extends Component {
-    render() {
+    componentDidMount () {
+        // this.init()
+        console.log(window.BMap)
+    }
+
+
+
+    render () {
+
+
         return (
-            <div>
-                地图组件
+            <div className="mapIndex">
+               <div id="container"></div>
             </div>
         )
     }
+    // init () {
+    //     const { BMap } = window;
+    //     console.log(window.BMap)
+    //     // 1. 创建地图实例
+    //     const map = new BMap.Map("container");
+    //     // 2. 地图定位的经纬度设置(天安门)
+    //     let point = new BMap.Point(116.404, 39.915);
+    //     // 3. 设置地图的位置和缩放级别
+    //     map.centerAndZoom(point, 15);
+    // }
+
 }
