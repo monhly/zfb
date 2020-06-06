7c20b98e3512c02c556d31725fd821ead2ba6996
15101321280 <1925659630@qq.com>
2020-06-07 00:01:25 +0800

    登录-使用yup进行登陆的验证

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -6,41 +6,18 @@ import { Link } from 'react-router-dom'
 import styles from './index.module.css'
 import { getLogin } from "../../request/login"
 import { withFormik } from 'formik';
+import * as Yup from 'yup';
 // 验证规则：
-// const REG_UNAME = /^[a-zA-Z_\d]{5,8}$/
-// const REG_PWD = /^[a-zA-Z_\d]{5,12}$/
+const REG_UNAME = /^[a-zA-Z_\d]{5,8}$/
+const REG_PWD = /^[a-zA-Z_\d]{5,12}$/
 
 class Login extends Component {
   state = {
     username:'',
     password:''
   }
-  // 阻止表单默认提交
-  onSubmits = (e) => {
-    e.preventDefault()
-    // 获取前的值
-    this.getLoginMsg()
-    // 发送axios请求
-  }
-  // 表单输入事件
-  onChanges = (e) => {
-    // 根据获取的name属性进行赋值
-    let code=e.target.name
-    this.setState({
-     [code]:e.target.value
-    })
 
-  }
   // 获取axios请求验证表单
- getLoginMsg=  async ()=> {
-    const {body,status} =await getLogin(this.state)
-    // 对获取的数据进行判断
-   if (status === 200) {
-      // 请求成功保存token
-   } else {
-     Toast.fail('密码或验证码错误',1)
-   }
-  }
   render() {
     const {
       values,
@@ -48,6 +25,7 @@ class Login extends Component {
       handleChange,
       handleSubmit,
     } = this.props;
+
     return (
       <div className={styles.root}>
         {/* 顶部导航 */}
@@ -69,7 +47,7 @@ class Login extends Component {
               />
             </div>
             {/* 长度为5到8位，只能出现数字、字母、下划线 */}
-            {/* <div className={styles.error}>账号为必填项</div> */}
+            {errors.username && <div className={styles.error}>{errors.username}</div>}
             <div className={styles.formItem}>
               <input
                 value={values.password}
@@ -81,7 +59,7 @@ class Login extends Component {
               />
             </div>
             {/* 长度为5到12位，只能出现数字、字母、下划线 */}
-            {/* <div className={styles.error}>账号为必填项</div> */}
+            {errors.password && <div className={styles.error}>{errors.password}</div>}
             <div className={styles.formSubmit}>
               <button className={styles.submit} type="submit">
                 登 录
@@ -102,27 +80,34 @@ class Login extends Component {
 
 export default  withFormik({
   mapPropsToValues: () => ({ username: '',password:'' }),
-
+  validationSchema: Yup.object().shape({
+    username: Yup.string().required('账号为必填项！').matches(REG_UNAME, '账号长度为5到8位，只能出现数字、字母、下划线'),
+    password: Yup.string().required('密码为必填项！').matches(REG_PWD, '密码长度为5到12位，只能出现数字、字母、下划线'),
+  }),
   // Custom sync validation
-  validate: values => {
-    const errors = {};
+  // validate: values => {
+  //   const errors = {};
 
-    if (!values.username) {
-      errors.username = 'Required';
-    }
+  //   if (!values.username) {
+  //     errors.username = 'Required';
+  //   }
 
-    return errors;
-  },
+  //   return errors;
+  // },
 
-  handleSubmit: async(value) => {
+  handleSubmit: async(value,formikBag) => {
   //
     // 获取表单的值
     // 进行数据的请求
+    console.log(formikBag);
+
     const { status,description, body } = await getLogin(value)
     // 对请求的数据进行判断
     if (status === 200) {
       window.localStorage.setItem('token', body.token)
       Toast.success(description, 2)
+      // 获取formikBag里面的props属性进行页面的跳转
+      formikBag.props.history.push('/home/user')
     } else {
       Toast.fail(description)
     }
