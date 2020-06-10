d327436a2d26a6f4b73f09f4ce63eb8cf64e9154
15101321280 <1925659630@qq.com>
2020-06-10 16:42:35 +0800

    地图找房-初步绘制遮盖物的样式

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { NavBar, Icon } from 'antd-mobile';
-import {getCurrCity}from"../../utils/getCity"
+import { getCurrCity } from "../../utils/getCity"
+// 导入样式
+import styles from"./index.module.css"
 export default class Map extends Component {
     componentDidMount () {
         this.init()
@@ -13,7 +15,7 @@ export default class Map extends Component {
         const map = new BMap.Map("container");
         const {  lable } = await getCurrCity();
        // 创建地址解析器实例
-        var myGeo = new BMap.Geocoder();
+        const myGeo = new BMap.Geocoder();
         // 将地址解析结果显示在地图上，并调整地图视野
         myGeo.getPoint(null, function(point){
             if (point) {
@@ -26,6 +28,24 @@ export default class Map extends Component {
                 map.addControl(new BMap.ScaleControl());
                 // 缩略地图
                 map.addControl(new BMap.OverviewMapControl());
+                // 添加遮罩层
+                const opts = {
+                    position : point,    // 指定文本标注所在的地理位置
+                    offset   : new BMap.Size(0,0)    //设置文本偏移量
+                }
+                // 实例文本对象的标注
+                const label = new BMap.Label(null, opts);  // 创建文本标注对象
+                label.setContent(
+                    `<div class="${styles.bubble}">
+                    <p class="${styles.bubbleName}">浦东新区</p>
+                    <p>388套</p>
+                  </div>`
+                )
+                label.setStyle({
+                    backgroundColor: 'transparent',
+                    border:'none'
+                });
+                  map.addOverlay(label);
                             }
         },
         lable);
