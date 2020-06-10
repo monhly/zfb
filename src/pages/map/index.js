fa39a76baaba7a2bd2dc2f03e455f73716452e47
15101321280 <1925659630@qq.com>
2020-06-10 20:53:16 +0800

    城市找房-实现对找房遮罩层数据的封装,点击切换区域实现房源信息的显示

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -12,70 +12,81 @@ export default class Map extends Component {
     }
     // 初始化地图
     init =async()=> {
-        const { BMap } = window;
-        // 1. 创建地图实例
-        const map = new BMap.Map("container");
+        // 挂载到实例上
+     this.BMap = window.BMap;
+    // 创建地图实例
+    this.map = new  this.BMap.Map("container");
         const { value, lable } = await getCurrCity();
        // 创建地址解析器实例
-        const myGeo = new BMap.Geocoder();
+        const myGeo = new  this.BMap.Geocoder();
         // 将地址解析结果显示在地图上，并调整地图视野
         myGeo.getPoint(null,async (point)=>{
             if (point) {
-                map.centerAndZoom(point, 11);
+                this.map.centerAndZoom(point, 11);
                 // 添加遮罩层
                 // 添加控件
                 // 添加平移的控件
-                map.addControl(new BMap.NavigationControl());
-                map.addControl(new BMap.ScaleControl());
-                // 发送请求获取数据
-                const {status,body} = await getInitHouse(value)
-                // 遍历遮罩层
-                if (status === 200) {
-                    body.forEach(item => {
-                        const {
-                            coord: { longitude, latitude },
-                            label: areaName,
-                            count,
-                            value
-                        } = item
-                            // 转换地理位置坐标
-                        const ipoint = new BMap.Point(longitude, latitude);
-                        // 绘制覆盖物
-                    const opts = {
-                        position: ipoint,    // 指定文本标注所在的地理位置
-                        offset: new BMap.Size(0, 0)    //设置文本偏移量
-                        }
-                     const label = new BMap.Label(null, opts);  // 创建文本标注对象
-                        label.setContent(
-                            `  <div class="${styles.bubble}">
-                            <p class="${styles.bubbleName}">${areaName}</p>
-                            <p>${count}</p>
-                            </div>`
-                        )
-                        label.setStyle({
-                            backgroundColor: 'transparent',
-                            border:'none'
-                        });
-                        // 设置遮罩层的点击事件
-                        label.addEventListener('click', function (e) {
-                        console.log('你好');
-
-                        })
-                            // 添加遮罩层
-                        map.addOverlay(label);
-                    })
-                }
-
-
-
-
+                this.map.addControl(new  this.BMap.NavigationControl());
+                this.map.addControl(new  this.BMap.ScaleControl());
+                this.renderOverLays(value)
 
                 }
             },
             lable);
     }
 
+    // 封装渲染遮罩层的方法
+    renderOverLays = async (id) => {
 
+         // 发送请求获取数据
+         const {status,body} = await getInitHouse(id)
+        // 遍历遮罩层
+        if (status === 200) {
+            body.forEach(item => {
+                const {
+                    coord: { longitude, latitude },
+                    label: areaName,
+                    count,
+                    value
+                } = item
+                    // 转换地理位置坐标
+                const ipoint = new  this.BMap.Point(longitude, latitude);
+                // 绘制覆盖物
+                const opts = {
+                position: ipoint,    // 指定文本标注所在的地理位置
+                offset: new  this.BMap.Size(0, 0)    //设置文本偏移量
+                }
+             const label = new  this.BMap.Label(null, opts);  // 创建文本标注对象
+                label.setContent(
+                    `  <div class="${styles.bubble}">
+                    <p class="${styles.bubbleName}">${areaName}</p>
+                    <p>${count}</p>
+                    </div>`
+                )
+                label.setStyle({
+                    backgroundColor: 'transparent',
+                    border:'none'
+                });
+                // 设置遮罩层的点击事件
+                label.addEventListener('click',  (e)=> {
+                    console.log('你好');
+                    this.renderOverLays(value)
+                        // 点击是销毁遮罩层
+                    let id=setTimeout(() => {
+                        //   清除定时器
+                        clearTimeout(id)
+                        // 清除遮罩层
+                        this.map.clearOverlays();
+                    })
+                    this.map.centerAndZoom(ipoint, 13);
+                    // 调用下一层
+
+                })
+                    // 添加遮罩层
+                this.map.addOverlay(label);
+            })
+        }
+    }
     render () {
 
 
